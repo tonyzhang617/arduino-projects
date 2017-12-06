950c3983f1e816d7c1ce31044f3eaf74c7902abf
tonyzhang617 <tonyzhang617@gmail.com>
2017-12-05 21:52:50 -0500

    Initial commit

---
+++ basic_led/basic_led.ino
@@ -0,0 +1,12 @@
+int ledPin = 13;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  delay(1000);                  // waits for a second
+  digitalWrite(ledPin, LOW);    // sets the LED off
+  delay(1000);                  // waits for a second
+}
